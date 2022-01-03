54af832364afd63e2bc76ac5e740e86a4533a0c5
Md Saddam Hossain <77723902+SaddamHossaain@users.noreply.github.com>
2022-01-03 21:40:36 +0600

    User in/out on server side check
    User connect and disconnect on Server Side checking function create.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express'); // ExprssJS import from none_modules
 const app = express();
 const http = require('http'); //import http
-const { hostname } = require('os');
 const createServer = http.createServer(app); //server create
+
 // Socket.io import and server
 const {Server} = require('socket.io');
 const io = new Server(createServer); //Object create of Server
+// User in/out detect
+io.on('connection', function(socket) {
+    console.log("new User connected!");
+    socket.on('disconnect', function() {
+        console.log('User Disconnected!');
+    })
+})
 
 // client side index.html file integrate
 app.get('/', function(req, res) {
@@ -13,6 +20,7 @@ app.get('/', function(req, res) {
 })
 
 // server run
+const { hostname } = require('os');
 createServer.listen(3000, hostname, function() {
     console.log('Server run @ 3000 port');
 });
